7c979a34e1240d197b6a4a689ef3f3938b025502
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-05 20:09:24 +0300

    Added gallery rendering from objects array

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRootEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(item =>
+  createGalleryItem({
+    previewImage: item.preview,
+    fullsizeImage: item.original,
+    description: item.description,
+  })
+);
+
+galleryRootEl.innerHTML = galleryMarkup.join(' ');
+
+function createGalleryItem({ previewImage, fullsizeImage, description }) {
+  return `<a class="gallery__item" href=${fullsizeImage}>
+            <img class="gallery__image" src=${previewImage} alt=${description} />
+        </a>`;
+}
