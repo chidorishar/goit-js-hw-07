f25b055e7200b0d2f40f9c80e5869232d2738303
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-05 19:57:10 +0300

    Created script for task 1 of homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRootEl = document.querySelector('.gallery');
+let modalWindow = null;
+
+galleryRootEl.addEventListener('click', onPictureClick);
+const galleryMarkup = galleryItems.map(item =>
+  createGalleryItem({
+    previewImage: item.preview,
+    fullsizeImage: item.original,
+    description: item.description,
+  })
+);
+
+galleryRootEl.innerHTML = galleryMarkup.join(' ');
+
+function createGalleryItem({ previewImage, fullsizeImage, description }) {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href=${fullsizeImage}>
+                <img
+                class="gallery__image"
+                src=${previewImage}
+                data-source=${fullsizeImage}
+                alt=${description}
+                />
+            </a>
+        </div>`;
+}
+
+function onPictureClick(event) {
+  const clickTarget = event.target;
+
+  event.preventDefault();
+  if (clickTarget.nodeName != 'IMG') {
+    return;
+  }
+
+  modalWindow = basicLightbox.create(`<img
+                src=${clickTarget.dataset.source}
+                alt=${clickTarget.alt}
+                />`);
+  modalWindow.show();
+  document.body.addEventListener('keydown', onKeyDown);
+}
+
+function onModalClose() {
+  document.body.removeEventListener('keydown', onKeyDown);
+}
+
+function onKeyDown(event) {
+  if (event.code != 'Escape') {
+    return;
+  }
+
+  modalWindow.close(onModalClose);
+}
